3e16b45012f9ffcf21ef628fbcadba7476c8471c
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-06-28 12:02:26 +0600

    feat: update title and header link to reflect personal branding

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -49,7 +49,7 @@ export default function Header() {
             whileHover={{ scale: 1.05 }}
             transition={{ type: "spring", stiffness: 400, damping: 10 }}
           >
-            <a href="#home">DevPortfolio</a>
+            <a href="#home">Abdullah Al Suad</a>
           </motion.div>
 
           {/* Desktop Navigation */}
