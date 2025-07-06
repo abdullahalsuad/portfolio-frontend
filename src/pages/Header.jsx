68d6a285a886f3d0ad468de249d7ae14b902af55
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-07-06 23:04:27 +0600

    up

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -19,7 +19,6 @@ export default function Header() {
     { name: "About", href: "#about" },
     { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
-    { name: "Experience", href: "#experience" },
     { name: "Contact", href: "#contact" },
   ];
 
